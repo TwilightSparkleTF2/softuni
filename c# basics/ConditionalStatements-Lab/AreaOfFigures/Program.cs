9867c046e8a6fd9c837f680906f926651a07680e
Twilight <93546000+TwilightSparkleTF2@users.noreply.github.com>
2025-11-17 17:31:34 +0200

    finished Conditional Statements – Lab

---
+++ c# basics/ConditionalStatements-Lab/AreaOfFigures/Program.cs
@@ -0,0 +1,27 @@
+﻿string type = Console.ReadLine();
+if (type == "square")
+{
+    double a = double.Parse(Console.ReadLine());
+    double solution1 = a * a;
+    Console.WriteLine(solution1);
+}
+else if (type == "rectangle")
+{
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+    double solution2 = b * c;
+    Console.WriteLine(solution2);
+}
+else if (type == "circle")
+{
+    double d = double.Parse(Console.ReadLine());
+    double solution3 = Math.PI * d * d;
+    Console.WriteLine(solution3);
+}
+else if (type == "triangle")
+{
+    double d = double.Parse(Console.ReadLine());
+    double e = double.Parse(Console.ReadLine());
+    double solution4 = (e * d) / 2;
+    Console.WriteLine(solution4);
+}
---
+++ c# basics/ConditionalStatements-Lab/PasswordGuess/Program.cs
@@ -0,0 +1,10 @@
+﻿string passoword = "s3cr3t!P@ssw0rd";
+string passwordImput = Console.ReadLine();
+if (passoword == passwordImput)
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
