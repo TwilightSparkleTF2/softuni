96046b0782104431c9486fd0b102fa94688f6654
Twilight <93546000+TwilightSparkleTF2@users.noreply.github.com>
2026-04-21 11:23:22 +0300

    started doing for() exercises

---
+++ c# basics/For Loop-Lab/SumNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    int chislo = int.Parse(Console.ReadLine());
+    sum += chislo;
+}
+Console.WriteLine(sum);
---
+++ c# basics/For Loop-Lab/VowelsSum/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+int num = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    char letter = input[i];
+	switch (letter)
+	{
+		case 'a':
+			num += 1;
+			break;
+        case 'e':
+            num += 2;
+            break;
+        case 'i':
+            num += 3;
+            break;
+        case 'o':
+            num += 4;
+            break;
+        case 'u':
+            num += 5;
+            break;
+    }
+}
+Console.WriteLine(num);
